e7bc6a581b204917dc714ac8e8590e1d5ac4260a
Xin Wang <xinwang7733@gmail.com>
2021-02-23 21:45:29 +1100

    added unit test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,191 @@
-console.log('Hello Accessible Tabs.');
+const KEYS = {
+    END: 35,
+    HOME: 36,
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+    DELETE: 46
+};
+
+document.querySelectorAll('.nav-tabs .nav-link').forEach(item => {
+    item.addEventListener('click', e => clickEventListener(e))
+    item.addEventListener('keyup', e => keyupEventListener(e))
+})
+
+export const clickEventListener = (e) => activateTab(e.target)
+
+const keyupEventListener = (e) => {
+    let key = e.keyCode
+    let currTab = e.target
+    switch (key) {
+        case KEYS.END:
+            activateLastTab(currTab)
+            break
+        case KEYS.HOME:
+            activateFirstTab(currTab)
+            break
+        case KEYS.LEFT:
+            activateLeftTab(currTab)
+            break
+        case KEYS.RIGHT:
+            activateRightTab(currTab)
+            break
+        case KEYS.UP:
+            activateUpTab(currTab)
+            break 
+        case KEYS.DOWN:
+            activateDownTab(currTab)
+            break 
+        case KEYS.DELETE:
+            deleteTab(currTab)
+            break                                    
+    }
+}
+
+export const activateLastTab = (currTab) => {
+    let tabs = getTabs(currTab)
+    if (tabs.length > 0) { activateTab(tabs[tabs.length-1]) }
+}
+
+const activateFirstTab = (currTab) => {
+    let tabs = getTabs(currTab)
+    if (tabs.length > 0) { activateTab(tabs[0]) }
+}
+
+const activateLeftTab = (currTab) => {
+    if (currTab.dataset.orientation != "vertical") {
+        switchTab(currTab, "decrease")
+    }
+}
+
+const activateRightTab = (currTab) => {
+    if (currTab.dataset.orientation != "vertical") {
+        switchTab(currTab)
+    }
+}
+
+const activateUpTab = (currTab) => {
+    if (currTab.dataset.orientation == "vertical") {
+        switchTab(currTab, "decrease")
+    }
+}
+
+const activateDownTab = (currTab) => {
+    if (currTab.dataset.orientation == "vertical") {
+        switchTab(currTab)
+    }
+}
+
+/**
+ * Switch tab with keyboard
+ * 
+ * @param {HTMLElement} currTab
+ * @param {string} dir 
+ */
+export const switchTab = (currTab, dir="increase") => {
+    let tabs = getTabs(currTab)
+    if (tabs && tabs.length > 1) {
+        let index = tabs.indexOf(currTab)
+        if (dir == "decrease") {
+            if (index > 0) {
+                activateTab(tabs[index - 1])
+            }
+        } else {
+            if (index < tabs.length - 1) {
+                activateTab(tabs[index + 1])
+            }
+        }
+    }
+}
+
+/**
+ * Activate a tab so it is highlighted and its corresponding pane is shown
+ * 
+ * @param {*} currTab 
+ */
+export const activateTab = (currTab) => {
+    currTab.focus()
+    updateTabs(currTab)
+    updatePanes(document.querySelector(currTab.dataset.targetPane))
+}
+
+/**
+ * Get all tabs in an array
+ * 
+ * @param {HTMLElement} currTab 
+ */
+const getTabs = (currTab) => getParentChildren(currTab, null).filter(e => hasClass(e,"nav-link"))
+
+/**
+ * Update the tabs status
+ * 
+ * @param {HTMLElement} currTab 
+ */
+const updateTabs = (currTab) => toggleActiveClass(currTab, "nav-link")
+
+/**
+ * Update the pane status
+ * 
+ * @param {HTMLElement} currPane 
+ */
+const updatePanes = (currPane) => {
+    toggleActiveClass(currPane, "tab-pane")
+}
+
+/**
+ * Get node siblings
+ * 
+ * @param {HTMLElement} node 
+ */
+const getSiblings = (node) => getParentChildren(node, node)
+
+const removeClass = (e, className) => e.classList.remove(className)
+
+const addClass = (e, className) => e.classList.add(className)
+
+const hasClass = (e, className) => e.classList.contains(className)
+
+/**
+ * Delete selected tab and it's according pane
+ * 
+ * @param {HTMLElement} currTab 
+ */
+export const deleteTab = (currTab) => {
+    let pane = document.querySelector(currTab.dataset.targetPane);
+    pane.parentElement.removeChild(pane);
+    currTab.parentElement.removeChild(currTab);
+};
+
+/**
+ * Only toggle the node with specified class name
+ * 
+ * @param {HTMLElement} node 
+ * @param {string} className 
+ */
+export const toggleActiveClass = (node, className) => {
+    addClass(node, "active");
+
+    getSiblings(node)
+        .filter(e => hasClass(e, className))
+        .forEach(e => removeClass(e, "active"))
+}
+
+/**
+ *  Return current node's parent's all children
+ *
+ *  @param  currNode    current node
+ *  @param  skipMe      the child to be escaped
+ * 
+ *  @return 
+ */
+export const getParentChildren = (currNode, skipMe) => {
+    let r = [];
+    let n = currNode.parentNode.firstChild
+
+    for (; n; n = n.nextSibling)
+        if (n.nodeType == 1 && n != skipMe)
+            r.push(n);
+
+    return r;
+};
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,3 +1,142 @@
-test('init', () => {
-    expect(1).toBe(1)
+import { activateTab, getParentChildren, switchTab, toggleActiveClass, deleteTab } from '../src/index'
+
+describe('index.js unit tests', () => {
+    // Set up our document body
+    document.body.innerHTML =
+        `
+        <div class="nav-tabs">
+            <button class="nav-link active" id="nav-home-tab" type="button" data-target-pane="#nav-home">Home</button>
+            <button class="nav-link" id="nav-profile-tab" type="button" data-target-pane="#nav-profile">Profile</button>
+            <button class="nav-link" id="nav-contact-tab" type="button" data-target-pane="#nav-contact">Contact</button>
+        </div>
+        <div class="tab-content">
+            <div class="tab-pane active" id="nav-home">home</div>
+            <div class="tab-pane" id="nav-profile">profile</div>
+            <div class="tab-pane" id="nav-contact">content</div>
+        </div>
+        <hr>
+        <div class="nav-vertical">
+            <div class="nav-tabs">
+                <button class="nav-link active" type="button" data-target-pane="#nav-home-v" data-orientation="vertical">Home</button><br>
+                <button class="nav-link" type="button" data-target-pane="#nav-profile-v" data-orientation="vertical">Profile</button><br>
+                <button class="nav-link" type="button" data-target-pane="#nav-contact-v" data-orientation="vertical">Contact</button><br>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane active" id="nav-home-v">home</div>
+                <div class="tab-pane" id="nav-profile-v">profile</div>
+                <div class="tab-pane" id="nav-contact-v">content</div>
+            </div>
+        </div>
+    `
+
+    /**
+     * Test getParentChildren function.
+     * 
+     * When this function is called with selected node, the selected node with its siblings will 
+     * be returned in an array.
+     */
+    test('getParentChildren', () => {
+        const currentNode = document.getElementById('nav-home-tab')
+        const children = getParentChildren(currentNode)
+
+        expect(children[0].getAttribute('id')).toBe('nav-home-tab')
+        expect(children[1].getAttribute('id')).toBe('nav-profile-tab')
+        expect(children[2].getAttribute('id')).toBe('nav-contact-tab')
+    })
+
+    /**
+     * Test activateTab function.  
+     * 
+     * When this function is called with selected tab, the active class will be added to this tab 
+     * and the other tab's active class will be removed.  And the according pane will be updated
+     * as well.
+     */
+    test('activateTab', () => {
+        const currentNode = document.getElementById('nav-profile-tab')
+
+        activateTab(currentNode)
+
+        expect(document.getElementById('nav-home-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile-tab').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-contact-tab').classList.contains("active")).toBe(false)
+
+        expect(document.getElementById('nav-home').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-contact').classList.contains("active")).toBe(false)
+    })
+
+    /**
+     * Test switchTab function.
+     * 
+     * This function is called when user hit the arrows keyboard.  It updates the selected tabs
+     * and shown panes based on the tab UI orientation and arrow keys.
+     */
+    test('switchTab', () => {
+        const currentNode = document.getElementById('nav-home-tab')
+
+        switchTab(currentNode)
+
+        expect(document.getElementById('nav-home-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-home').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile-tab').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-profile').classList.contains("active")).toBe(true)
+
+        const currentNode2 = document.getElementById('nav-profile-tab')
+
+        switchTab(currentNode2, "decrease")
+
+        expect(document.getElementById('nav-home-tab').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-home').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-profile-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile').classList.contains("active")).toBe(false)
+    })
+
+    /**
+     * Test switchTab function.
+     * 
+     * This function should do nothing when the current tab is the first tab and user
+     * clicks the left arraw.
+     */
+    test('switchTab should do nothing when user clicks the left arraw key when current tab is the first tab', () => {
+        const currentNode = document.getElementById('nav-home-tab')
+
+        switchTab(currentNode, "decrease")
+
+        expect(document.getElementById('nav-home-tab').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-home').classList.contains("active")).toBe(true)
+        expect(document.getElementById('nav-profile-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-contact-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-contact').classList.contains("active")).toBe(false)
+    })
+
+    /**
+     * Test toggleActiveClass function.
+     * 
+     * This function is used when a tab or a pane should be activated, accordingly the other tabs
+     * or panes should be de-activated
+     */
+    test('toggleActiveClass', () => {
+        const currentNode = document.getElementById('nav-contact-tab')
+
+        toggleActiveClass(currentNode, "nav-link")
+
+        expect(document.getElementById('nav-home-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-profile-tab').classList.contains("active")).toBe(false)
+        expect(document.getElementById('nav-contact-tab').classList.contains("active")).toBe(true)
+    })
+
+    /**
+     * Test deleteTab function.
+     * 
+     * This function is called when user selected a tab and hit delete key
+     */
+    test('deleteTab', () => {
+        const currentNode = document.getElementById('nav-contact-tab')
+
+        deleteTab(currentNode)
+
+        expect(document.getElementById('nav-contact-tab')).toBe(null)
+        expect(document.getElementById('nav-contact')).toBe(null)
+    })
 });
